3d1ee00f7ec871ae9bfa3547990bf2bc4b6f673f
Andy Stanberry <andystanberry@gmail.com>
2013-04-08 11:06:10 -0400

    Now with more python

--- pymple/pymple.py
+++ pymple/pymple.py
@@ -1,10 +1,11 @@
 class Pymple(dict):
-    def __getitem__(self, id):
-        value = self.raw(id)
+    def __getitem__(self, key):
+        value = self.raw(key)
 
         return value(self) if callable(value) else value
 
-    def share(self, callback):
+    @staticmethod
+    def share(callback):
         def inner(c):
             if not hasattr(inner, 'object'):
                 inner.object = callback(c)
@@ -12,26 +13,22 @@ class Pymple(dict):
             return inner.object
         return inner
 
-    def extend(self, id, callback):
-        factory = self.raw(id)
+    def extend(self, key, callback):
+        factory = self.raw(key)
 
         if not callable(factory):
-            raise KeyError('Identifier "%s" does not contain an object definition.' % (id))
+            raise KeyError('Identifier "%s" does not contain an object definition.' % (key))
 
-        def inner(c):
-            return callback(factory(c), c)
+        self[key] = lambda c: callback(factory(c), c)
 
-        self[id] = inner
+        return self[key]
 
-        return self[id]
+    def raw(self, key):
+        if key not in self:
+            raise KeyError('Ientifier "%s" is not defined.' % (key))
 
-    def raw(self, id):
-        if id not in self:
-            raise KeyError('Ientifier "%s" is not defined.' % (id))
+        return super(Pymple, self).__getitem__(key)
 
-        return super(Pymple, self).__getitem__(id)
-
-    def protect(self, callback):
-        def inner(c):
-            return callback
-        return inner
+    @staticmethod
+    def protect(callback):
+        return lambda c: callback
