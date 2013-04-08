8841e1012ebc7760a5d58af761b2391c848bfaac
Andy Stanberry <andystanberry@gmail.com>
2013-04-08 11:39:34 -0400

    Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pymple",
-    version="1.0",
+    version="1.1",
     author="Andy Stanberry",
     author_email="andystanberry@gmail.com",
     url="https://github.com/cranberyxl/pymple",
